8f77220ee2acb7f5ebd97f45a078ef0681c189c7
Philip Dye <phdye@acm.org>
2025-06-08 22:09:34 -0400

    Fix paths, eliminate use of globals

--- src/wrapper/wrapper.c
+++ src/wrapper/wrapper.c
@@ -16,23 +16,24 @@
 #define ENV_VAR_PID_FMT           "%s__PID"             /* <name> */
 #define ENV_VAR_PID_PATH_FMT      "%s__PID_PATH"        /* <name> */
 
-#define TRANSLATED_FILE           "translated.txt"
+typedef struct {
+    char current_exe        [PATH_MAX];
+    char bin_dir            [PATH_MAX];
+    char target_exe         [PATH_MAX];
+    char prefix             [PATH_MAX];
+    char exe_name           [PATH_MAX];
+    char env_var_name       [PATH_MAX];
+    char env_var_pid        [PATH_MAX];
+    char env_var_pid_value  [PATH_MAX]; // for setting the environment variable
+    char pid_path           [PATH_MAX];
+} context_t ;
 
-// populated by identity()
-char current_dir[PATH_MAX];
-char prefix[PATH_MAX];
-char current_exe[PATH_MAX];
-char exe_name[PATH_MAX];
-char env_var_name[PATH_MAX];
-char env_var_pid[PATH_MAX];
-char env_var_pid_value[PATH_MAX]; // for setting the environment variable
-char pid_path[PATH_MAX];
 int parent_pid = 0;
 
-void record_command_line(int argc, char *argv[]);
-void identity();
-void environment(char * prefix);
-int locate_target_executable(char *target_executable);
+void identity(context_t * ctx);
+void environment(context_t * ctx);
+int locate_target_exe(context_t * ctx);
+
 int is_executable(const char *path);
 char *strtoupper(const char *str);
 void perrorf(char * fmt, ...);
@@ -41,80 +42,102 @@ void strip_extension(char *str);
 
 int main(int argc, char *argv[]) {
 
-    record_command_line(argc, argv); // fopen failure will exit
+    context_t ctx;
 
     // Make stdout unbuffered
     unbuffer(stdout); // continue even if it fails
 
-    identity(); // exit on failure : realpath, setenv
+    identity(&ctx); // exit on failure : realpath, setenv
 
-    environment(prefix);
+    environment(&ctx);
 
     // Locate the target executable
-    char target_executable[PATH_MAX];
-    if (locate_target_executable(target_executable) != EXIT_SUCCESS) {
+    if (locate_target_exe(&ctx) != EXIT_SUCCESS) {
         return EXIT_FAILURE;
     }
 
-    if ( strcmp(target_executable, current_exe) == 0 ) {
+    if ( strcmp(ctx.target_exe, ctx.current_exe) == 0 ) {
         fprintf(stderr, "Error: Target executable is the same as the wrapper executable:  '%s'\n",
-            target_executable);
+            ctx.target_exe);
         return EXIT_FAILURE;
     }
 
-    execvp(target_executable, argv);
+    execvp(ctx.target_exe, argv);
 
-    perrorf("execvp('%s', ...)", target_executable);
+    perrorf("execvp('%s', ...)", ctx.target_exe);
     return EXIT_FAILURE;
 }
 
 // Populate globals with program executable identity
 //
-void identity() {
+void identity(context_t* ctx) {
 
-    if (getcwd(current_dir, sizeof(current_dir)) != NULL) {
-        printf("Current working dir    :  '%s'\n", current_dir);
-    } else {
-        perror("getcwd() error");
-        abort();
-    }
+    char parent[PATH_MAX];
+    char tmp_path[PATH_MAX];
 
     // Get the path of the command executable
-    if (realpath(PROC_SELF_EXE, current_exe) == NULL) {
+    if (realpath(PROC_SELF_EXE, ctx->current_exe) == NULL) {
         perrorf("realpath('%s') failed\n", PROC_SELF_EXE);
         exit(EXIT_FAILURE);
     }
-    printf("Current executable     :  '%s'\n", current_exe);
+    printf("Current executable     :  '%s'\n", ctx->current_exe);
 
-    strncpy(exe_name, basename(current_exe), PATH_MAX);
-    printf("Executable basename    :  ''%s'\n", exe_name);
+    strncpy(ctx->exe_name, basename(ctx->current_exe), PATH_MAX);
+    printf("Executable basename    :  ''%s'\n", ctx->exe_name);
 
-    strncpy(prefix, dirname(dirname(current_exe)), PATH_MAX);
-    printf("Prefix                 :  '%s'\n", prefix);
+    printf("Current executable     :  '%s'\n", ctx->current_exe);
 
-    snprintf(env_var_name, PATH_MAX, ENV_VAR_NAME_FMT, exe_name);
-    snprintf(env_var_pid, PATH_MAX, ENV_VAR_PID_FMT, env_var_name);
+    strncpy(tmp_path, ctx->current_exe, PATH_MAX);
+    strncpy(ctx->bin_dir, dirname(tmp_path), PATH_MAX);
+    printf("Bin dir                :  ''%s'\n", ctx->bin_dir);
+    printf("Current executable     :  '%s'\n", ctx->current_exe);
 
-    char *value = getenv(env_var_pid);
+    strncpy(tmp_path, ctx->bin_dir, PATH_MAX);
+    strncpy(ctx->prefix, dirname(tmp_path), PATH_MAX);
+    printf("Prefix                 :  '%s'\n", ctx->prefix);
+    printf("Current executable     :  '%s'\n", ctx->current_exe);
+
+    snprintf(ctx->env_var_name, PATH_MAX, ENV_VAR_NAME_FMT, ctx->exe_name);
+    snprintf(ctx->env_var_pid, PATH_MAX, ENV_VAR_PID_FMT, ctx->env_var_name);
+
+    char *value = getenv(ctx->env_var_pid);
     parent_pid = (value != NULL) ? atoi(value) : 0;
 
     if (parent_pid < 0) {
         // Set PID for child process
-        sprintf(env_var_pid_value, "%d", getpid());
-        if (setenv(env_var_pid, env_var_pid_value, 1) != 0) {
-            fprintf(stderr, "Error: setting environment variable '%s'\n", env_var_pid);
+        sprintf(ctx->env_var_pid_value, "%d", getpid());
+        if (setenv(ctx->env_var_pid, ctx->env_var_pid_value, 1) != 0) {
+            fprintf(stderr, "Error: setting environment variable '%s'\n", ctx->env_var_pid);
             perror("setenv");
             exit(EXIT_FAILURE);
         }
     }
+
+    printf("Current executable     :  '%s'\n", ctx->current_exe);
 }
 
-void environment(char * prefix) {
+int locate_target_exe(context_t* ctx) {
+
+    char candidate[PATH_MAX];
+
+    snprintf(candidate, sizeof(candidate), "%s/actual/%s", ctx->bin_dir, ctx->exe_name);
+    printf("Candidate              :  '%s'\n", candidate);
+    if (realpath(candidate, ctx->target_exe) != NULL && is_executable(ctx->target_exe)) {
+        return 0; // Found <name>.exe
+    }
+
+    // Fatal error if no target is found
+    fprintf(stderr, "Fatal Error: Could not locate a valid target executable for wrapped '%s'.\n",
+        ctx->current_exe);
+    return -1;
+}
+
+void environment(context_t * ctx) {
     char lib[PATH_MAX];
     char *current_path, *new_path;
     size_t size;
 
-    snprintf(lib, PATH_MAX, "%s/lib", prefix);
+    snprintf(lib, PATH_MAX, "%s/lib", ctx->prefix);
 
     printf("Library                :  '%s'\n", lib);
     current_path = getenv("LD_LIBRARY_PATH");
@@ -130,6 +153,7 @@ void environment(char * prefix) {
     printf("LD_LIBRARY_PATH        :  '%s'\n", new_path);
 
     setenv("LD_LIBRARY_PATH", new_path, 1);
+    setenv("PIP_DISABLE_PIP_VERSION_CHECK", "1", 1);
 }
 
 // export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
@@ -154,40 +178,6 @@ char * get_current_executable() {
     path[length] = '\0';
     
     return path;
-}
-
-// Write command line to TRANSLATED_FILE
-void record_command_line(int argc, char *argv[]) {
-    FILE * fp = fopen(TRANSLATED_FILE, "a");
-    if (fp == NULL) {
-        perrorf("fopen('%s', 'a')", TRANSLATED_FILE);
-        exit(EXIT_FAILURE);
-    }
-    fputs("Command line:", fp);
-    for (int i = 0; i < argc; ++i) {
-        fprintf(fp, " '%s'", argv[i]);
-    }
-    fprintf(fp, "\n\n");
-    fclose(fp);
-}
-
-int locate_target_executable(char *target_executable) {
-
-    char candidate[PATH_MAX];
-    char subdir[PATH_MAX];
-
-    snprintf(subdir, sizeof(subdir), "%s/actual", current_dir);
-
-    snprintf(candidate, sizeof(candidate), "%s/%s", subdir, exe_name);
-    printf("Candidate              :  '%s'\n", candidate);
-    if (realpath(candidate, target_executable) != NULL && is_executable(target_executable)) {
-        return 0; // Found <name>.exe
-    }
-
-    // Fatal error if no target is found
-    fprintf(stderr, "Fatal Error: Could not locate a valid target executable for wrapped '%s'.\n",
-        current_exe);
-    return -1;
 }
 
 int is_executable(const char *path) {
