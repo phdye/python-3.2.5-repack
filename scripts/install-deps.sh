f35123c4b5cdb0cd3429259d719bcec8d736ce28
Philip Dye <phdye@acm.org>
2025-06-09 06:27:05 -0400

    Comment out ncurses and readline

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -16,13 +16,13 @@ export BUILD_SUBJECT=python-deps
 
 source scripts/builder.sh
 
-# Compression and Security
+# Compression
 build zlib     1.2.13  https://zlib.net/fossils/zlib-1.2.13.tar.gz -xzf ""
 build bzip2    1.0.8   https://sourceware.org/pub/bzip2/bzip2-1.0.8.tar.gz -xzf ""
 
-# Interactive Shell Support
-build ncurses  6.4     https://ftp.gnu.org/gnu/ncurses/ncurses-6.4.tar.gz -xzf ""
-build readline 8.2     https://ftp.gnu.org/gnu/readline/readline-8.2.tar.gz -xzf ""
+# Interactive Shell Support -- oddly, this now breaks the build
+# build ncurses  6.4     https://ftp.gnu.org/gnu/ncurses/ncurses-6.4.tar.gz -xzf ""
+# build readline 8.2     https://ftp.gnu.org/gnu/readline/readline-8.2.tar.gz -xzf ""
 
 # Storage Backends
 build sqlite   3.45.3  https://www.sqlite.org/2024/sqlite-autoconf-3450300.tar.gz -xzf ""
