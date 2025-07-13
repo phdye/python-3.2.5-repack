0b782b8ff0616fb9ebd7edf549a12c0954af25d4
Philip Dye <phdye@acm.org>
2025-07-13 01:32:04 -0400

    scripts/build-sysroot.sh -- add 'python' symlink

--- scripts/build-sysroot.sh
+++ scripts/build-sysroot.sh
@@ -19,4 +19,8 @@ export LD_LIBRARY_PATH=
 
 ./scripts/populate-site-packages.sh
 
+# ( cd ~/sysroot/bin && ln -s python3.2 python3 )
+
+( cd ~/sysroot/bin && if [ ! -e python ] ; then ln -s python3.2 python ; fi )
+
 ./scripts/apply-portable-executable-wrapper.sh
