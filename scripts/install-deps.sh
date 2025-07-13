24eea3533314e2ff86c5441859ab28d062e0b842
Philip Dye <phdye@acm.org>
2025-07-13 01:32:44 -0400

    scripts/install-deps.sh -- uncomment steps

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -6,11 +6,11 @@ export BUILD_SUBJECT=python-deps
 
 #----------------------------------------------------------------------------
 
-# sudo apt update
+sudo apt update
 
-# sudo apt upgrade
+sudo apt upgrade
 
-# sudo apt install -y build-essential libc6-dev
+sudo apt install -y build-essential libc6-dev
 
 #----------------------------------------------------------------------------
 
@@ -21,8 +21,8 @@ build zlib     1.2.13  https://zlib.net/fossils/zlib-1.2.13.tar.gz -xzf ""
 build bzip2    1.0.8   https://sourceware.org/pub/bzip2/bzip2-1.0.8.tar.gz -xzf ""
 
 # Interactive Shell Support -- oddly, this now breaks the build
-# build ncurses  6.4     https://ftp.gnu.org/gnu/ncurses/ncurses-6.4.tar.gz -xzf ""
-# build readline 8.2     https://ftp.gnu.org/gnu/readline/readline-8.2.tar.gz -xzf ""
+build ncurses  6.4     https://ftp.gnu.org/gnu/ncurses/ncurses-6.4.tar.gz -xzf ""
+build readline 8.2     https://ftp.gnu.org/gnu/readline/readline-8.2.tar.gz -xzf ""
 
 # Storage Backends
 build sqlite   3.45.3  https://www.sqlite.org/2024/sqlite-autoconf-3450300.tar.gz -xzf ""
