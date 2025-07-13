b852c635c8efd57968dbb7d1a1fb7ac219731843
Philip Dye <phdye@acm.org>
2025-07-13 08:49:53 -0400

    build-deps.sh: restore bzip2 ncurses readline

--- scripts/build-deps.sh
+++ scripts/build-deps.sh
@@ -10,13 +10,18 @@ source scripts/builder.sh
 
 # Compression
 build zlib     1.2.13  https://zlib.net/fossils/zlib-1.2.13.tar.gz -xzf ""
-# build bzip2    1.0.8   https://sourceware.org/pub/bzip2/bzip2-1.0.8.tar.gz -xzf ""
+build bzip2    1.0.8   https://sourceware.org/pub/bzip2/bzip2-1.0.8.tar.gz -xzf ""
 
 # Interactive Shell Support -- oddly, this now breaks the build
-# NO # build ncurses  6.4     https://ftp.gnu.org/gnu/ncurses/ncurses-6.4.tar.gz -xzf ""
-# NO # build readline 8.2     https://ftp.gnu.org/gnu/readline/readline-8.2.tar.gz -xzf ""
-# Use System # build readline 8.2     https://ftp.gnu.org/gnu/readline/readline-8.2.tar.gz -xzf ""
-#   ... otherwise awk dies due to 'UP' feature missing.
+build ncurses  6.4     https://ftp.gnu.org/gnu/ncurses/ncurses-6.4.tar.gz -xzf \
+    "--with-shared --without-ada --without-debug --with-termlib"
+
+SAVE_LDFLAGS="${LDFLAGS}"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -Wl,--no-as-needed -ltinfo"
+build readline 8.2     https://ftp.gnu.org/gnu/readline/readline-8.2.tar.gz -xzf \
+    "--with-curses"
+LDFLAGS="${SAVE_LDFLAGS}"
+
 
 # Storage Backends
 build sqlite   3.45.3  https://www.sqlite.org/2024/sqlite-autoconf-3450300.tar.gz -xzf ""
