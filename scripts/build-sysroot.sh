7cf6fb8c312ae38351277dddbba5c1e142833403
Philip Dye <phdye@acm.org>
2025-07-13 05:42:10 -0400

    build-sysroot.sh:  clean slate every run

--- scripts/build-sysroot.sh
+++ scripts/build-sysroot.sh
@@ -9,18 +9,18 @@ export LD_LIBRARY_PATH=
 
 # ./scripts/diagnose-env.sh
 
+( set -x && rm -rf ~/sysroot && mkdir ~/sysroot )
+
 ./scripts/install-deps.sh
+
+./scripts/build-deps.sh
 
 ./scripts/build-openssl.sh
 
-./scripts/build-gdbm.sh
+# Now handled in install-deps.sh # ./scripts/build-gdbm.sh
 
 ./scripts/build-python.sh
 
 ./scripts/populate-site-packages.sh
-
-# ( cd ~/sysroot/bin && ln -s python3.2 python3 )
-
-( cd ~/sysroot/bin && if [ ! -e python ] ; then ln -s python3.2 python ; fi )
 
 ./scripts/apply-portable-executable-wrapper.sh
